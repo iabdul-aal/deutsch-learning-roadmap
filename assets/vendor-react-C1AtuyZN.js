6e0f09a0c810f7c336da5711906178a494194e2a
Islam Ibrahim <islamibr@outlook.com>
2026-08-11 22:17:16 +0300

    Updates

--- assets/vendor-react-C1AtuyZN.js
+++ assets/vendor-react-C1AtuyZN.js
@@ -1,4 +1,4 @@
-import{r as _a}from"./vendor-icons-fZ5GYUxs.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{r as _a}from"./vendor-icons-Bq8yAlxA.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
