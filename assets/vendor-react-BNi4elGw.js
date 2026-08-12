570e7e2c2250611cc6868213f09af66371b12bff
Islam Ibrahim <islamibr@outlook.com>
2026-08-12 21:12:07 +0300

    Updates

--- assets/vendor-react-BNi4elGw.js
+++ assets/vendor-react-BNi4elGw.js
@@ -1,4 +1,4 @@
-import{r as _a}from"./vendor-icons-DePnal6Z.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{r as _a}from"./vendor-icons-CJ5uFkay.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
